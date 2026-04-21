f20ec824cb91f5f3abdc7a21d84dc7ba548e5a89
joaosaturnino <joao.saturnino@etec.sp.gov.br>
2026-04-21 17:45:35 -0300

    teste

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Importação de todas as páginas
 import Home from './pages/Home';
 import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 // Se tiver criado o CSS global, descomente a linha abaixo:
-// import './assets/global.css';
+import './assets/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
